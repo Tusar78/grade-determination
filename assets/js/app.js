0a74a4d18c91da72ecc847f9ebb7b7877bd86c10
Tusar <tusarahmed141@gmail.com>
2022-01-27 11:28:33 +0600

    Final code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,20 @@
-const userValue = document.getElementById('input').value;
-
-if (userValue >= 90 && userValue >= 100) {
+function myFunc() {    
+const userValue = document.getElementById('inp').value;
+let result = document.getElementById('demo');
+if ((userValue >= 90) && (userValue >= 100)) {
     console.log('A+');
+    result.innerHTML = 'A+';
 } else if (userValue >= 80) {
     console.log('B');
+    result.innerHTML = 'B';
 } else if (userValue >= 70) {
     console.log('C');
+    result.innerHTML = 'c';
 } else if (userValue >= 60) {
     console.log('D');
-} else if (userValue >= 50) {
+    result.innerHTML = 'D';
+} else if (userValue <= 50) {
     console.log('F');
+    result.innerHTML = 'F';
+}
 }
